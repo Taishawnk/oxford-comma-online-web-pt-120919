3766440b76949021f617559b23a6261e23603871
Taishawn King <taishawnking1@yahoo.com>
2019-12-18 00:02:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+# require"pry"
+# def oxford_comma(array)
+  
+# array.map {|string| string}.join("")
+# p array
+# binding.pry 
 
+# end
+
+def oxford_comma(array)
+   if array.length <= 2 
+    return array.join(" and ")
+ elsif array.length >= 3
+     array[-1].insert(0, "and ")
+     return array.join(", ")
+  end
 end
